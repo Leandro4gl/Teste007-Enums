1fb36d9d6e4b160873e294983357c0d8dc5836b9
Leandro de Oliveira <leandro4gl@gmail.com>
2020-04-17 10:58:37 -0300

    Criação do Projeto (zerado)

---
+++ Teste007-Enums/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste007_Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
